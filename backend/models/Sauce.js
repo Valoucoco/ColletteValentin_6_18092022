7f21653414d3ae018ac457223b5c2cefaaeb1e7a
Valoucoco <102355003+Valoucoco@users.noreply.github.com>
2022-09-29 14:11:05 +0200

    modification de routes dans sauce.js

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -6,7 +6,6 @@ const sauceSchema = mongoose.Schema({
     description: { type: String, required: true },
     imageUrl: { type: String, required: true },
     userId: { type: String, required: true },
-    price: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
